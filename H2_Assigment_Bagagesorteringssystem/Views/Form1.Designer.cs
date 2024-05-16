bb7d82b5484d600215e441b0e9353152a0cb4c67
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2024-05-16 10:07:54 +0200

    s

--- H2_Assigment_Bagagesorteringssystem/Views/Form1.Designer.cs
+++ H2_Assigment_Bagagesorteringssystem/Views/Form1.Designer.cs
@@ -42,10 +42,38 @@
 			this.label2 = new System.Windows.Forms.Label();
 			this.panel11 = new System.Windows.Forms.Panel();
 			this.panel12 = new System.Windows.Forms.Panel();
+			this.panel18 = new System.Windows.Forms.Panel();
+			this.panel19 = new System.Windows.Forms.Panel();
+			this.panel20 = new System.Windows.Forms.Panel();
+			this.panel21 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel22 = new System.Windows.Forms.Panel();
+			this.panel23 = new System.Windows.Forms.Panel();
+			this.panel24 = new System.Windows.Forms.Panel();
+			this.panel13 = new System.Windows.Forms.Panel();
+			this.panel14 = new System.Windows.Forms.Panel();
+			this.panel15 = new System.Windows.Forms.Panel();
+			this.label3 = new System.Windows.Forms.Label();
+			this.panel16 = new System.Windows.Forms.Panel();
+			this.panel17 = new System.Windows.Forms.Panel();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.panel25 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
 			this.panel1.SuspendLayout();
 			this.panel5.SuspendLayout();
 			this.panel8.SuspendLayout();
 			this.panel9.SuspendLayout();
+			this.panel19.SuspendLayout();
+			this.panel20.SuspendLayout();
+			this.panel13.SuspendLayout();
+			this.panel14.SuspendLayout();
+			this.panel25.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// panel2
@@ -77,9 +105,9 @@
 			this.label1.AutoSize = true;
 			this.label1.Location = new System.Drawing.Point(3, 11);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(52, 13);
+			this.label1.Size = new System.Drawing.Size(55, 13);
 			this.label1.TabIndex = 3;
-			this.label1.Text = "Checkin1";
+			this.label1.Text = "Checkin 1";
 			// 
 			// panel1
 			// 
@@ -89,7 +117,7 @@
 			this.panel1.Controls.Add(this.panel3);
 			this.panel1.Location = new System.Drawing.Point(67, 172);
 			this.panel1.Name = "panel1";
-			this.panel1.Size = new System.Drawing.Size(141, 114);
+			this.panel1.Size = new System.Drawing.Size(132, 114);
 			this.panel1.TabIndex = 0;
 			// 
 			// panel5
@@ -125,7 +153,7 @@
 			this.panel8.Controls.Add(this.panel11);
 			this.panel8.Location = new System.Drawing.Point(67, 299);
 			this.panel8.Name = "panel8";
-			this.panel8.Size = new System.Drawing.Size(141, 114);
+			this.panel8.Size = new System.Drawing.Size(132, 114);
 			this.panel8.TabIndex = 5;
 			// 
 			// panel9
@@ -150,9 +178,9 @@
 			this.label2.AutoSize = true;
 			this.label2.Location = new System.Drawing.Point(3, 11);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(52, 13);
+			this.label2.Size = new System.Drawing.Size(55, 13);
 			this.label2.TabIndex = 3;
-			this.label2.Text = "Checkin1";
+			this.label2.Text = "Checkin 2";
 			// 
 			// panel11
 			// 
@@ -165,10 +193,199 @@
 			// panel12
 			// 
 			this.panel12.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-			this.panel12.Location = new System.Drawing.Point(246, 80);
+			this.panel12.Location = new System.Drawing.Point(245, 80);
 			this.panel12.Name = "panel12";
-			this.panel12.Size = new System.Drawing.Size(206, 40);
+			this.panel12.Size = new System.Drawing.Size(189, 40);
 			this.panel12.TabIndex = 3;
+			// 
+			// panel18
+			// 
+			this.panel18.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel18.Location = new System.Drawing.Point(900, 483);
+			this.panel18.Name = "panel18";
+			this.panel18.Size = new System.Drawing.Size(141, 38);
+			this.panel18.TabIndex = 6;
+			// 
+			// panel19
+			// 
+			this.panel19.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.panel19.Controls.Add(this.panel20);
+			this.panel19.Controls.Add(this.label4);
+			this.panel19.Controls.Add(this.panel22);
+			this.panel19.Location = new System.Drawing.Point(936, 407);
+			this.panel19.Name = "panel19";
+			this.panel19.Size = new System.Drawing.Size(105, 114);
+			this.panel19.TabIndex = 5;
+			// 
+			// panel20
+			// 
+			this.panel20.BackColor = System.Drawing.Color.Red;
+			this.panel20.Controls.Add(this.panel21);
+			this.panel20.Location = new System.Drawing.Point(61, 11);
+			this.panel20.Name = "panel20";
+			this.panel20.Size = new System.Drawing.Size(19, 60);
+			this.panel20.TabIndex = 4;
+			// 
+			// panel21
+			// 
+			this.panel21.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel21.Location = new System.Drawing.Point(147, 0);
+			this.panel21.Name = "panel21";
+			this.panel21.Size = new System.Drawing.Size(121, 338);
+			this.panel21.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(3, 11);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(56, 13);
+			this.label4.TabIndex = 3;
+			this.label4.Text = "Terminal 2";
+			// 
+			// panel22
+			// 
+			this.panel22.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel22.Location = new System.Drawing.Point(147, 0);
+			this.panel22.Name = "panel22";
+			this.panel22.Size = new System.Drawing.Size(121, 338);
+			this.panel22.TabIndex = 2;
+			// 
+			// panel23
+			// 
+			this.panel23.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel23.Location = new System.Drawing.Point(863, 80);
+			this.panel23.Name = "panel23";
+			this.panel23.Size = new System.Drawing.Size(37, 441);
+			this.panel23.TabIndex = 3;
+			// 
+			// panel24
+			// 
+			this.panel24.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel24.Location = new System.Drawing.Point(669, 80);
+			this.panel24.Name = "panel24";
+			this.panel24.Size = new System.Drawing.Size(200, 40);
+			this.panel24.TabIndex = 4;
+			// 
+			// panel13
+			// 
+			this.panel13.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.panel13.Controls.Add(this.panel14);
+			this.panel13.Controls.Add(this.label3);
+			this.panel13.Controls.Add(this.panel16);
+			this.panel13.Location = new System.Drawing.Point(936, 172);
+			this.panel13.Name = "panel13";
+			this.panel13.Size = new System.Drawing.Size(105, 114);
+			this.panel13.TabIndex = 7;
+			// 
+			// panel14
+			// 
+			this.panel14.BackColor = System.Drawing.Color.Red;
+			this.panel14.Controls.Add(this.panel15);
+			this.panel14.Location = new System.Drawing.Point(61, 11);
+			this.panel14.Name = "panel14";
+			this.panel14.Size = new System.Drawing.Size(19, 60);
+			this.panel14.TabIndex = 4;
+			// 
+			// panel15
+			// 
+			this.panel15.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel15.Location = new System.Drawing.Point(147, 0);
+			this.panel15.Name = "panel15";
+			this.panel15.Size = new System.Drawing.Size(121, 338);
+			this.panel15.TabIndex = 2;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 11);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(56, 13);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "Terminal 1";
+			// 
+			// panel16
+			// 
+			this.panel16.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel16.Location = new System.Drawing.Point(147, 0);
+			this.panel16.Name = "panel16";
+			this.panel16.Size = new System.Drawing.Size(121, 338);
+			this.panel16.TabIndex = 2;
+			// 
+			// panel17
+			// 
+			this.panel17.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.panel17.Location = new System.Drawing.Point(900, 248);
+			this.panel17.Name = "panel17";
+			this.panel17.Size = new System.Drawing.Size(141, 38);
+			this.panel17.TabIndex = 8;
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(424, 172);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(257, 445);
+			this.listView1.TabIndex = 9;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// panel25
+			// 
+			this.panel25.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.panel25.Controls.Add(this.label5);
+			this.panel25.Location = new System.Drawing.Point(424, 43);
+			this.panel25.Name = "panel25";
+			this.panel25.Size = new System.Drawing.Size(257, 114);
+			this.panel25.TabIndex = 5;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(92, 47);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(74, 13);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "SortingSystem";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.pictureBox1.ImageLocation = "C:\\Users\\zbc23jakl\\source\\repos\\DevNicklas\\H2_Assigment_Bagagesorteringssystem\\H2" +
+    "_Assigment_Bagagesorteringssystem\\Views\\airplane.png";
+			this.pictureBox1.Location = new System.Drawing.Point(1065, 86);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 10;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+			this.pictureBox2.ImageLocation = "C:\\Users\\zbc23jakl\\source\\repos\\DevNicklas\\H2_Assigment_Bagagesorteringssystem\\H2" +
+    "_Assigment_Bagagesorteringssystem\\Views\\airplane.png";
+			this.pictureBox2.Location = new System.Drawing.Point(1065, 321);
+			this.pictureBox2.Margin = new System.Windows.Forms.Padding(0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(200, 200);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 11;
+			this.pictureBox2.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(1282, 321);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 12;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(1282, 86);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 13;
 			// 
 			// MainForm
 			// 
@@ -176,6 +393,18 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.Color.White;
 			this.ClientSize = new System.Drawing.Size(1833, 913);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.panel25);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.panel17);
+			this.Controls.Add(this.panel13);
+			this.Controls.Add(this.panel24);
+			this.Controls.Add(this.panel23);
+			this.Controls.Add(this.panel18);
+			this.Controls.Add(this.panel19);
 			this.Controls.Add(this.panel12);
 			this.Controls.Add(this.panel7);
 			this.Controls.Add(this.panel4);
@@ -190,7 +419,18 @@
 			this.panel8.ResumeLayout(false);
 			this.panel8.PerformLayout();
 			this.panel9.ResumeLayout(false);
+			this.panel19.ResumeLayout(false);
+			this.panel19.PerformLayout();
+			this.panel20.ResumeLayout(false);
+			this.panel13.ResumeLayout(false);
+			this.panel13.PerformLayout();
+			this.panel14.ResumeLayout(false);
+			this.panel25.ResumeLayout(false);
+			this.panel25.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
@@ -209,6 +449,27 @@
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Panel panel11;
 		private System.Windows.Forms.Panel panel12;
+		private System.Windows.Forms.Panel panel18;
+		private System.Windows.Forms.Panel panel19;
+		private System.Windows.Forms.Panel panel20;
+		private System.Windows.Forms.Panel panel21;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel panel22;
+		private System.Windows.Forms.Panel panel23;
+		private System.Windows.Forms.Panel panel24;
+		private System.Windows.Forms.Panel panel13;
+		private System.Windows.Forms.Panel panel14;
+		private System.Windows.Forms.Panel panel15;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Panel panel16;
+		private System.Windows.Forms.Panel panel17;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Panel panel25;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
 	}
 }
 
