6c0641bc6ac13360a4b83c080eb9c70bc9d5b3d7
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-05-16 09:49:50 +0200

    s

---
+++ H2_Assigment_Bagagesorteringssystem/Models/Terminal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Assigment_Bagagesorteringssystem.Models
+{
+    internal class Terminal
+    {
+        private bool _status = false;
+
+        internal bool Status
+        {
+            get
+            {
+                return _status;
+            }
+        }
+
+        internal void ReceiveFromSorting(Baggage baggage)
+        {
+
+        }
+
+        internal void Open()
+        {
+
+        }
+
+        internal void Close()
+        {
+
+        }
+    }
+}
