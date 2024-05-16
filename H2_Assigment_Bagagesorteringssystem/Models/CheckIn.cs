2521c55e93f49567cfaba287b0909e45dfbcb901
janickelev@gmail.com <janickelev@gmail.com>
2024-05-16 15:54:16 +0200

    s

--- H2_Assigment_Bagagesorteringssystem/Models/CheckIn.cs
+++ H2_Assigment_Bagagesorteringssystem/Models/CheckIn.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using H2_Assigment_Bagagesorteringssystem.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace H2_Assigment_Bagagesorteringssystem.Models
@@ -10,6 +12,10 @@ namespace H2_Assigment_Bagagesorteringssystem.Models
 	{
         private bool _status = false;
 
+
+        /// <summary>
+        /// Gets the status of the check-in counter (open/closed).
+        /// </summary>
         internal bool Status
         {
             get
@@ -20,14 +26,55 @@ namespace H2_Assigment_Bagagesorteringssystem.Models
 
 		internal CheckIn(int inventorySize) : base(inventorySize) { }
 
+
+        /// <summary>
+        /// Opens the check-in counter.
+        /// </summary>
 		internal void Open()
         {
-
+            _status = true;
         }
 
+
+        /// <summary>
+        /// Closes the check-in counter.
+        /// </summary>
         internal void Close()
         {
-
+            _status = false;
         }
+
+
+        /// <summary>
+        /// Services a passenger by processing their baggage and sending it to the sorting system.
+        /// </summary>
+        /// <param name="baggage">The baggage to be processed.</param>
+        internal void ServicePassenger(Baggage baggage)
+        {
+            GetBaggage(baggage);
+            Thread.Sleep(100);
+            SendBaggageToSortingSystem();
+        }
+
+        /// <summary>
+        /// Adds baggage to the check-in inventory.
+        /// </summary>
+        internal void GetBaggage(Baggage baggage)
+        {
+            this.AddToInventory(baggage);
+        }
+
+        /// <summary>
+        /// Sends baggage from the check-in inventory to the sorting system for further processing.
+        /// </summary>
+        internal void SendBaggageToSortingSystem()
+        {
+            Baggage baggage = this.RemoveFromInventory();
+            if (baggage != null)
+            {
+                Airport.SortingSystems[0].EnqueueBaggage(baggage);
+            }
+        }
+
     }
 }
