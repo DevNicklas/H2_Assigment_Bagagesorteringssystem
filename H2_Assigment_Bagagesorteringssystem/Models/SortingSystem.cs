6639388ba47d5b81d4fd309d8f3538ff779881bc
janickelev@gmail.com <janickelev@gmail.com>
2024-05-16 15:41:33 +0200

    s

--- H2_Assigment_Bagagesorteringssystem/Models/SortingSystem.cs
+++ H2_Assigment_Bagagesorteringssystem/Models/SortingSystem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using H2_Assigment_Bagagesorteringssystem;
 using H2_Assigment_Bagagesorteringssystem.Controllers;
@@ -10,14 +11,55 @@ namespace H2_Assigment_Bagagesorteringssystem.Models
 {
     internal class SortingSystem: InventoryContainer
     {
-        private Queue<Baggage> inputQueue;
+        private Queue<Baggage> _inputQueue;
+        private readonly object _lock = new object(); // Lock object for synchronization
+        /// <summary>
+        /// Initiates the baggage sorting system and begins processing incoming baggage.
+        /// </summary>
+        internal void StartSystem()
+		{
+            ProcessBaggage();
+        }
 
-		/// <summary>
-		/// Sorts the given baggage to the appropriate terminal based on its flight number.
-		/// If the baggage cannot be sorted to any terminal, it is enqueued in the input queue.
-		/// </summary>
-		/// <param name="baggage">The baggage to be sorted.</param>
-		internal void Sort(Baggage baggage)
+        /// <summary>
+        /// Processes the incoming baggage items from the input queue.
+        /// Dequeues baggage items from the input queue and sorts them using the Sort method.
+        /// </summary>
+        private void ProcessBaggage()
+        {
+            while (true)
+            {
+                Baggage baggage = null;
+                lock (_lock) // Locking to ensure thread safety
+                {
+                    if (_inputQueue.Count > 0)
+                    {
+                        baggage = _inputQueue.Dequeue();
+                    }
+                }
+                if (baggage != null)
+                {
+                    Sort(baggage);
+                }
+                Thread.Sleep(200);
+            }
+        }
+
+        /// <summary>
+        /// Adds a new baggage item to the input queue for sorting.
+        /// </summary>
+        /// <param name="baggage">The baggage item to be added to the input queue.</param>
+        internal void EnqueueBaggage(Baggage baggage)
+        {
+            _inputQueue.Enqueue(baggage);
+        }
+
+        /// <summary>
+        /// Sorts the given baggage to the appropriate terminal based on its flight number.
+        /// If the baggage cannot be sorted to any terminal, it is enqueued in the input queue.
+        /// </summary>
+        /// <param name="baggage">The baggage to be sorted.</param>
+        internal void Sort(Baggage baggage)
 		{
 			foreach (Terminal terminal in Airport.Terminals)
 			{
@@ -29,8 +71,8 @@ namespace H2_Assigment_Bagagesorteringssystem.Models
 					}
 				}
 			}
-			// If the baggage couldn't be sorted to any terminal, enqueue it in the input queue
-			inputQueue.Enqueue(baggage);
+            // If the baggage couldn't be sorted to any terminal, enqueue it in the input queue
+            _inputQueue.Enqueue(baggage);
 		}
 		internal SortingSystem(int inventorySize) : base(inventorySize) { }
     }
