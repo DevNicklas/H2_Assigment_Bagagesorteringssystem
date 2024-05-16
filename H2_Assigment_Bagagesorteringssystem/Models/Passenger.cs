974c8e91f3db53f607f8d06a6bb66c556b1efb6a
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2024-05-16 12:29:38 +0200

    s

---
+++ H2_Assigment_Bagagesorteringssystem/Models/Passenger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H2_Assigment_Bagagesorteringssystem.Models
+{
+    internal class Passenger
+    {
+        private string firstName = "Undefined";
+        private string lastName = "Undefined";
+        private string passportNumber = "Undefined";
+        private string boardingPassNumber = "Undefined";
+
+        internal Passenger()
+        {
+
+        }
+    }
+}
