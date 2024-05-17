b319f1f2d12a4baa6289b66da3bced0951120b91
JanickLarsen <141632187+JanickLarsen@users.noreply.github.com>
2024-05-17 09:09:27 +0200

    s

--- H2_Assigment_Bagagesorteringssystem/Controllers/Airport.cs
+++ H2_Assigment_Bagagesorteringssystem/Controllers/Airport.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -56,8 +57,35 @@ namespace H2_Assigment_Bagagesorteringssystem.Controllers
                 return _status;
             }
         }
+		internal static void RunAirport()
+		{
 
-        internal static bool ChangeStatus()
+            AddCheckIn();
+            AddTerminal();
+
+            Thread threadCheckin = new Thread();
+            Thread threadTerminal = new Thread(Terminal.);
+		}
+		internal static void RunCheckIn()
+		{
+			
+			while (true)
+            {
+                Baggage baggage = new Baggage();
+                checkIn.ServicePassenger(baggage);
+
+			}
+		}
+		internal static void RunTerminal()
+		{
+			Terminal Terminal = new Terminal(1);
+			while (true)
+			{
+
+			}
+		}
+
+		internal static bool ChangeStatus()
         {
             if(_status)
             {
@@ -69,10 +97,9 @@ namespace H2_Assigment_Bagagesorteringssystem.Controllers
             }
             return _status;
         }
-
         internal static void AddPlane(int size)
         {
-			//_planes.Add(new Plane(size));
+			_planes.Add(new Plane(50, "New York", 180, new DateTime(2024, 5, 17, 14, 30, 0));
 		}
         internal static void AddSortingSystem()
         {
--- H2_Assigment_Bagagesorteringssystem/Views/Form1.cs
+++ H2_Assigment_Bagagesorteringssystem/Views/Form1.cs
@@ -6,16 +6,19 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using H2_Assigment_Bagagesorteringssystem.Models;
 namespace H2_Assigment_Bagagesorteringssystem
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
-            InitializeComponent();
+            Airport.RunAirport();
+
+			InitializeComponent();
         }
 
         private void toggleAirportStatusBtn_Click(object sender, EventArgs e)
